86314ece4bda172a6f6676c5a32e1d0dbb8600c4
EC2 Default User <ec2-user@ip-172-31-0-210.ap-northeast-1.compute.internal>
2023-12-16 09:13:38 +0000

    add homes items genres customers orders order_details controller

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,19 @@
+class Admin::ItemsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
