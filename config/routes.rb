51a8dfd9dba05e1cb6870e21609c835a5eb85cf8
KouichiTsushima <147828512+TsuKou@users.noreply.github.com>
2023-12-16 09:13:10 +0000

    Add public/controller, Add registarations/new.html add code

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,41 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'addresses/index'
+    get 'addresses/edit'
+    get 'addresses/create'
+    get 'addresses/update'
+    get 'addresses/destroy'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/confirm'
+    get 'orders/thanks'
+    get 'orders/create'
+    get 'orders/index'
+    get 'orders/show'
+  end
+  namespace :public do
+    get 'car_items/index'
+    get 'car_items/update'
+    get 'car_items/destroy'
+    get 'car_items/destroy_all'
+    get 'car_items/create'
+  end
+  namespace :public do
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/upadte'
+    get 'customers/check'
+    get 'customers/withdraw'
+  end
+  namespace :public do
+    get 'items/index'
+    get 'items/show'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
