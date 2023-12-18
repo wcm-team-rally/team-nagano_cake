9edc3e37bbada9dacaea48635122e40c6a76620e
KouichiTsushima <147828512+TsuKou@users.noreply.github.com>
2023-12-18 05:41:26 +0000

    Add&Make top&about-page with controller,routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   namespace :admin do
     get 'order_details/update'
   end
@@ -31,46 +31,23 @@ Rails.application.routes.draw do
     get 'homes/top'
   end
 
-
-
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-    get 'addresses/create'
-    get 'addresses/update'
-    get 'addresses/destroy'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/thanks'
-    get 'orders/create'
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'car_items/index'
-    get 'car_items/update'
-    get 'car_items/destroy'
-    get 'car_items/destroy_all'
-    get 'car_items/create'
-  end
-  
   scope module: :public do
-   get "customers/mypage" => "customers#show"
-   get "customers/information/edit" => "customers#edit"
-   patch "customers/information" => "customers#update"
-   get "customers/check" => "customers#check"
-   patch "customers/withdraw" => "customers#withdraw"
-  end
-  
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
+    root to: "homes#top"
+
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    resources :orders, only: [:new, :create, :index, :show]
+    resources :items, only: [:index, :show]
+    resources :car_items, only: [:index, :update, :destroy, :create]
+
+    get "about" => "homes#about", as: "about"
+    post 'orders/confirm' => "orders#confirm"
+    get 'orders/thanks' => "orders#thanks"
+    delete "car_items/destroy_all" => "car_items#destroy_all"
+    get "customers/mypage" => "customers#show"
+    get "customers/information/edit" => "customers#edit"
+    patch "customers/information" => "customers#update"
+    get "customers/check" => "customers#check"
+    patch "customers/withdraw" => "customers#withdraw"
   end
   # 顧客用
 # URL /customers/sign_in ...
@@ -84,6 +61,6 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
