6fab9aac029d77d704f147c8481ea642cf9b37d3
EC2 Default User <ec2-user@ip-172-31-14-137.ap-northeast-1.compute.internal>
2023-12-20 10:08:33 +0000

    Correct-shipping_address

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,7 +2,7 @@ class Public::AddressesController < ApplicationController
   before_action :authenticate_customer!
   
   def index
-    @addresses = Address.all
+    @addresses = current_customer.addresses
     @address = Address.new
   end
   
@@ -12,9 +12,9 @@ class Public::AddressesController < ApplicationController
     if @address.save
       redirect_to addresses_path
     else
-      @addresses = Address.all
+      @addresses = current_customer.addresses
       render :index
-      
+    end
   end
 
   def edit
