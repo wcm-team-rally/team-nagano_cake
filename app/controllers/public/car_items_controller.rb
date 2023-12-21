ca294b015c705a2cce7f942c25cef398267e68d4
EC2 Default User <ec2-user@ip-172-31-0-210.ap-northeast-1.compute.internal>
2023-12-21 03:34:55 +0000

    cart-items

--- app/controllers/public/car_items_controller.rb
+++ app/controllers/public/car_items_controller.rb
@@ -1,50 +1,55 @@
 class Public::CarItemsController < ApplicationController
   before_action :authenticate_customer!
-  before_action :set_car_item, only: [:create, :update, :destroy]
-
+  
   def index
-    @car_items = current_customer.car_items.includes(:item)
+    @car_items = CarItem.all
+    @item = Item.all
+    @total = @car_items.inject(0) { |sum, car_item| sum + car_item.subtotal }
+
   end
 
   def create
-    if @car_item
-      new_amount = @car_item.amount + car_item_params[:amount]
-      @car_item.update(amount: new_amount)
-      redirect_to car_items_path
-    else
-      @car_item = current_customer.car_items.new(car_item_params)
-      @car_item.item_id = @item.id
-      if @car_item.save
+    if current_customer.car_items.find_by(item_id: params[:car_item][:item_id]).present?
+      @car_item = current_customer.car_items.find_by(item_id: params[:car_item][:item_id])
+      @cart_item.quantity += params[:cart_item][:quantity].to_i
+      @car_item.save
+        flash[:notice] = 'カートに商品が入りました。'
         redirect_to car_items_path
+    else
+      @car_item = CarItem.new(cart_item_params)
+      @car_item.customer_id=current_customer.id
+      if @car_item.save
+          flash[:notice] = 'カートに商品が入りました。'
+          redirect_to car_items_path
       else
-        render 'public/items/show'
+        flash[:alert] = '商品の追加に失敗しました。'
+          redirect_to item_path
       end
     end
   end
 
   def update
-    @car_item.update(car_item_params) if @car_item
-    redirect_to car_items_path
+    @car_item = CarItem.find_by(item_id: params[:car_item][:item_id])
+		@car_item.update(car_item_params)
+		redirect_to public_car_items_path
   end
 
   def destroy
-    @car_item.destroy if @car_item
-    redirect_to car_items_path
+    @car_item = CarItem.find(params[:id])
+    car_item.destroy
+    @car_items = CarItem.all
+  　render 'index'
   end
 
   def destroy_all
+    @car_items = CarItems.all
     current_customer.car_items.destroy_all
-    redirect_to car_items_path
+  　render 'index'
   end
 
   private
 
   def car_item_params
-    params.require(:car_item).permit(:amount)
-  end
-
-  def set_car_item
-    @item = Item.find(params[:item_id])
-    @car_item = current_customer.has_in_car(@item)
+    params.require(:car_item).permit(:item_id, :quantity, :customer_id)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,9 +1,15 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page])
+    #@items = Item.all
   end
 
   def show
-    @item = Item.find(paramas[:id])
+    @item = Item.find(params[:id])
+    @car_item = CarItem.all
+  end
+  
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :image, :customer_id, :item_id)
   end
 end
