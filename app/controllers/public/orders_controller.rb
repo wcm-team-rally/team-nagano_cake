d8a3c3ed1360fc79c8a326086d49d66a6633a54e
EC2 Default User <ec2-user@ip-172-31-14-137.ap-northeast-1.compute.internal>
2023-12-21 11:04:57 +0000

    Add-customer_order_showindex

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -49,9 +49,12 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+    @orders = Order.where(customer_id: current_customer.id).order(created_at: :desc).
   end
 
   def show
+    @order = Order.find(params[:id])
+    @order_details= OrderDetail.where(order_id: @order.id)
   end
   
   private
